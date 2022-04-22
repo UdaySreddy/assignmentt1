e1ded79a08287aa83c666c5a96b60ef93c3a67c9
root <udayshankarkolamaram.com>
2022-04-22 19:15:51 +0000

    errors reduced

--- app.js
+++ app.js
@@ -26,43 +26,103 @@ let initializeDB = async () => {
 };
 initializeDB();
 
+function modlist(x) {
+  let result = x.map((each) => {
+    let y = {};
+    y.id = each.id;
+    y.todo = each.todo;
+    y.priority = each.priority;
+    y.status = each.status;
+    y.category = each.category;
+    y.dueDate = each.due_date;
+    return y;
+  });
+  return result;
+}
+
 //get api
 app.get("/todos/", async (request, response) => {
   const { id, todo, category, priority, status, search_q } = request.query;
   let query1 = "";
+
   console.log(id, todo, category, priority, status, search_q);
   if (status !== undefined) {
     query1 = `select * from todo where status = "${status}";`;
+    z = await db.all(query1);
+    console.log(z.length);
+    if (z.length === 0) {
+      response.status(400);
+      response.send("Invalid Todo Status");
+    } else {
+      response.send(modlist(z));
+    }
   } else if (priority !== undefined) {
     query1 = `select * from todo where priority = "${priority}";`;
+    z = await db.all(query1);
+    if (z.length === 0) {
+      response.status(400);
+      response.send("Invalid Todo Priority");
+    } else {
+      response.send(modlist(z));
+    }
   } else if (status !== undefined && priority !== undefined) {
     query1 = `select * from todo where priority = "${priority}" and 
         status = "${status}";`;
+    z = await db.all(query1);
+    if (z.length === 0) {
+      response.status(400);
+      response.send("Invalid Todo Priority");
+    } else {
+      response.send(modlist(z));
+    }
   } else if (category !== undefined && status !== undefined) {
     query1 = `select * from todo where category = "${category}" and 
         status = "${status}";`;
+    z = await db.all(query1);
+    if (z.length === 0) {
+      response.status(400);
+      response.send("Invalid Todo Priority");
+    } else {
+      response.send(modlist(z));
+    }
   } else if (category !== undefined) {
     query1 = `select * from todo where category = "${category}";`;
+    z = await db.all(query1);
+    if (z.length === 0) {
+      response.status(400);
+      response.send("Invalid Todo Category");
+    } else {
+      response.send(modlist(z));
+    }
   } else if (category !== undefined && priority !== undefined) {
     query1 = `select * from todo where category = "${category}" and 
        priority = "${priority}";`;
+    z = await db.all(query1);
+    if (z.length === 0) {
+      response.status(400);
+      response.send("Invalid Todo Priority");
+    } else {
+      response.send(modlist(z));
+    }
   } else if (search_q !== undefined) {
     query1 = `select * from todo where todo like  "%${search_q}%";`;
+    z = await db.all(query1);
+    if (z.length === 0) {
+      response.status(400);
+      response.send("Invalid Todo Priority");
+    } else {
+      response.send(modlist(z));
+    }
   } else {
     query1 = `select * from todo;`;
+    z = await db.all(query1);
+    if (z.length === 0) {
+      response.status(400);
+      response.send("Invalid Todo Priority");
+    } else {
+      response.send(modlist(z));
+    }
   }
-  let x = await db.all(query1);
-  let result1 = x.map((each) => {
-    let x = {};
-    x.id = each.id;
-    x.todo = each.todo;
-    x.priority = each.priority;
-    x.status = each.status;
-    x.category = each.category;
-    x.dueDate = each.due_date;
-    return x;
-  });
-  response.send(result1);
 });
 
 //get params
@@ -93,18 +153,22 @@ app.get("/agenda/", async (request, response) => {
 
   let query3 = `select * from todo where due_date = "${fdate}";`;
   let x = await db.all(query3);
-  let result3 = x.map((each) => {
-    let x = {};
-    x.id = each.id;
-    x.todo = each.todo;
-    x.priority = each.priority;
-    x.status = each.status;
-    x.category = each.category;
-    x.dueDate = each.due_date;
-    return x;
-  });
-
-  response.send(result3);
+  if (x.length !== 0) {
+    let result3 = x.map((each) => {
+      let x = {};
+      x.id = each.id;
+      x.todo = each.todo;
+      x.priority = each.priority;
+      x.status = each.status;
+      x.category = each.category;
+      x.dueDate = each.due_date;
+      return x;
+    });
+    response.send(result3);
+  } else {
+    response.status(400);
+    response.send("Invalid Due Date");
+  }
 });
 
 //post todo
@@ -112,10 +176,10 @@ app.get("/agenda/", async (request, response) => {
 app.post("/todos/", async (request, response) => {
   const { id, todo, category, priority, status, dueDate } = request.body;
   let fdate = format(new Date(dueDate), "yyyy-M-dd");
-  let query4 = `insert into todo 
+  let querypost = `insert into todo 
   (id, todo, category, priority, status, due_date)
   values (${id},"${todo}","${category}","${priority}","${status}","${fdate}")`;
-  let x = await db.run(query4);
+  await db.run(querypost);
   response.send("Todo Successfully Added");
 });
 
@@ -123,25 +187,42 @@ app.put("/todos/:todoId/", async (request, response) => {
   const { id, todo, category, priority, status, dueDate } = request.body;
   let query5 = "";
   if (status !== undefined) {
-    query5 = `update todo set status = "${status}"`;
-    await db.run(query5);
-    response.send("Status Updated");
+    if (status === "TO DO" || status === "IN PROGRESS" || status === "DONE") {
+      query5 = `update todo set status = "${status}"`;
+      let a1 = await db.run(query5);
+      response.send("Status Updated");
+    } else {
+      response.status(400);
+      response.send("Invalid Todo Status");
+    }
   } else if (category !== undefined) {
-    query5 = `update todo set category = "${category}"`;
-    await db.run(query5);
-    response.send("Category Updated");
+    if (category === "WORK" || category === "HOME" || category === "LEARNING") {
+      query5 = `update todo set category = "${category}"`;
+      await db.run(query5);
+      response.send("Category Updated");
+    } else {
+      response.status(400);
+      response.send("Invalid Todo Category");
+    }
   } else if (priority !== undefined) {
-    query5 = `update todo set priority = "${priority}"`;
-    await db.run(query5);
-    response.send("Priority Updated");
+    if (priority === "HIGH" || priority === "MEDIUM" || priority === "LOW") {
+      query5 = `update todo set priority = "${priority}"`;
+      await db.run(query5);
+      response.send("Priority Updated");
+    } else {
+      response.status(400);
+      response.send("Invalid Todo Priority");
+    }
   } else if (todo !== undefined) {
     query5 = `update todo set todo = "${todo}"`;
     await db.run(query5);
     response.send("Todo Updated");
-  } else {
+  } else if (dueDate !== undefined) {
     query5 = `update todo set due_date = "${dueDate}"`;
     await db.run(query5);
     response.send("Due Date Updated");
+  } else {
+    console.log("x");
   }
 });
 
